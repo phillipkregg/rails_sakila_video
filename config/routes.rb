40adbc68a437309eb551aacd37bb6368744c979d
phillipkregg <phillipkregg@gmail.com>
2020-05-30 22:01:11 -0500

    Added a films route, and removed unnecessary config from database

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'films/index'
   resources :posts
+  resources :films
   root 'home#index'
-  #get 'home/index'
+  # get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
